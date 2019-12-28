586cb991cf9194297d5769f4ef5cd6bcd18191be
Everyone IDE <59180614+everyone-ide@users.noreply.github.com>
2019-12-28 13:10:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+function benefitsScroll(){
+  document.getElementById('benefits').scrollIntoView({behavior: 'smooth'});
+}
+function downloadScroll(){
+  document.getElementById('download').scrollIntoView({behavior: 'smooth'});
+}
