12880b8da1bf6f09f6509b17d09b55419cd96589
Everyone IDE <59180614+everyone-ide@users.noreply.github.com>
2019-12-30 22:08:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var li1 = document.getElementById("li1");
 var li2 = document.getElementById("li2");
 var li3 = document.getElementById("li3");
+var li4 = document.getElementById("li4");
 
 function benefitsScroll(){
   document.getElementById('benefits').scrollIntoView({behavior: 'smooth'});
