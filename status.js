c98224c71e549b85bdda346f6158f4fa2ef899bd
Everyone IDE <59180614+everyone-ide@users.noreply.github.com>
2019-12-31 20:55:35 +0100

    Update status.js

--- status.js
+++ status.js
@@ -3,5 +3,5 @@ var div = document.getElementById("status-div");
 var p = document.createElement("P");
 p.id = "status-p";
 p.classList.add("lead");
-p.innerHTML = "The developers are working on nav bar animations.";
+p.innerHTML = "The developers are currently doing nothing.";
 div.appendChild(p);
