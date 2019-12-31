0e6c2d8a05eaa48c47ca76a35fbe90a3448d537f
Everyone IDE <59180614+everyone-ide@users.noreply.github.com>
2019-12-31 20:16:03 +0100

    Update status.js

--- status.js
+++ status.js
@@ -2,5 +2,6 @@ var div = document.getElementById("status-div");
 
 var p = document.createElement("P");
 p.id = "status-p";
+p.classList.add("lead");
 p.innerHTML = "The developers are doing nothing currently.";
 div.appendChild(p);
