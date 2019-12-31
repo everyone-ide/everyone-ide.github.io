d9535361bf7be5864379fb0d0744bdafed0cd5c0
Everyone IDE <59180614+everyone-ide@users.noreply.github.com>
2020-01-01 00:45:01 +0100

    Update status.js

--- status.js
+++ status.js
@@ -1,7 +1,17 @@
 var div = document.getElementById("status-div");
 
+var i = document.createElement("I");
+i.id = "status-i"
+i.classList.add("fas fa-ban")
+i.style = "width: 15px; height: 15px; color: red;"
+div.appendChild(i);
+
+// fas fa-ban - No status available. - red
+// fas fa-laptop-code - Adding new feature. - lightblue
+// fas fa-bug - Fixing bugs. - green
+
 var p = document.createElement("P");
 p.id = "status-p";
 p.classList.add("lead");
-p.innerHTML = "The CEO is currently fixing a bug on the News page.";
+p.innerHTML = "No status available.";
 div.appendChild(p);
