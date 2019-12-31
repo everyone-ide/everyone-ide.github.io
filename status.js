8c16ddcaab03ee48f0590443e9406d8ea602af06
Everyone IDE <59180614+everyone-ide@users.noreply.github.com>
2020-01-01 00:39:58 +0100

    Update status.js

--- status.js
+++ status.js
@@ -3,5 +3,5 @@ var div = document.getElementById("status-div");
 var p = document.createElement("P");
 p.id = "status-p";
 p.classList.add("lead");
-p.innerHTML = "The CEO is currently working on a new article.";
+p.innerHTML = "The CEO is currently fixing a bug on the News page.";
 div.appendChild(p);
