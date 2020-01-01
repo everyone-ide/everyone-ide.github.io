724537766d57e4309e355a0af3e6fb5eabd300a7
Everyone IDE <59180614+everyone-ide@users.noreply.github.com>
2020-01-01 02:29:22 +0100

    Update status.js

--- status.js
+++ status.js
@@ -27,21 +27,26 @@ hr2.id = "status-hr2";
 hr2.classList.add("my-4");
 div.appendChild(hr2);
 
+var hr3 = document.createElement("HR");
+hr3.id = "status-hr3";
+hr3.classList.add("my-4");
+div.appendChild(hr3);
+
 var p2 = document.createElement("P");
 p2.id = "status-p2";
 p2.classList.add("lead");
 p2.innerHTML = "Technical";
 div.appendChild(p2);
 
-var hr3 = document.createElement("HR");
-hr3.id = "status-hr3";
-hr3.classList.add("my-4");
-div.appendChild(hr3);
-
 var hr4 = document.createElement("HR");
 hr4.id = "status-hr4";
 hr4.classList.add("my-4");
 div.appendChild(hr4);
+
+var hr5 = document.createElement("HR");
+hr5.id = "status-hr5";
+hr5.classList.add("my-4");
+div.appendChild(hr5);
 
 var p3 = document.createElement("P");
 p3.id = "status-p3"
@@ -50,7 +55,7 @@ p3.style = "color: #737373;";
 p3.innerHTML = "Both developers are doing nothing.";
 div.appendChild(p3);
 
-var hr5 = document.createElement("HR");
-hr5.id = "status-hr5"
-hr5.classList.add("my-4");
-div.appendChild(hr5);
+var hr6 = document.createElement("HR");
+hr6.id = "status-hr6"
+hr6.classList.add("my-4");
+div.appendChild(hr6);
